2f3493f5481c1b91a4a7ec31e9d383406288c77e
natthdias <nathalia.dias@nubank.com.br>
2021-12-15 13:39:21 -0300

    Montando o layout do formulário

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,37 @@ class FormularioTransferencia extends StatelessWidget {
       appBar: AppBar(
         title: Text('Criando Transferencia'),
       ),
-      body: Text('teste'),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              style: TextStyle(fontSize: 24.0),
+              decoration: InputDecoration(
+                labelText: 'Número da Conta',
+                hintText: '0000',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              style: TextStyle(fontSize: 24.0),
+              decoration: InputDecoration(
+                icon: Icon(Icons.monetization_on),
+                labelText: 'Valor',
+                hintText: '0.00',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Confirmar'),
+          )
+        ],
+      ),
     );
   }
 }
